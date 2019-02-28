580a7a2c95454414c36444b467efe36edf15dae4
Max Li <likongyang18@gmail.com>
2019-02-28 17:23:21 +0800

    add loaded

--- src/test_xueqiu.py
+++ src/test_xueqiu.py
@@ -6,6 +6,7 @@ import pytest
 
 
 class testXueqiu(unittest.TestCase):
+    load = False
 
     def setUp(self):
         caps = {}
@@ -16,23 +17,35 @@ class testXueqiu(unittest.TestCase):
         caps["autoGrantPermissions"] = "true"
         caps["automationName"] = "UiAutomator2"
 
+        # if load == True:
+        #     caps["noReset"] = "true"
         self.driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
         self.driver.implicitly_wait(10)
+        load = True
+
+    def loaded(self):
+        waitCondition = ["x", "y"]
+        while waitCondition[-1] != waitCondition[-2]:
+            elLocation = self.driver.find_element_by_xpath("//*[@text='自选']").location
+            waitCondition.append(elLocation)
 
     def test_add_pdd(self):
-        self.driver.find_element_by_xpath("//*[contains(@text, '搜索股票')]").click()
+        self.loaded()
+        self.driver.find_element_by_id("com.xueqiu.android:id/home_search").click()
         self.driver.find_element_by_id("com.xueqiu.android:id/search_input_text").send_keys("pdd")
-        if len(self.driver.find_elements_by_id("follow_btn")) > 0:
-            self.driver.find_element_by_id("follow_btn").click()
+        if len(self.driver.find_elements_by_id("com.xueqiu.android:id/follow_btn")) > 0:
+            self.driver.find_element_by_id("com.xueqiu.android:id/follow_btn").click()
             self.driver.find_element_by_xpath("//*[@text='下次再说']").click()
 
     def test_pdd_exist(self):
+        self.loaded()
         self.driver.find_element_by_xpath(
             "//*[@text='自选' and contains(@resource-id, 'tab_name')]").click()
         elText = self.driver.find_element_by_id("com.xueqiu.android:id/portfolio_stockName")
         assert elText.text == "拼多多"
 
     def test_search_add(self):
+        self.loaded()
         self.driver.find_element_by_xpath(
             "//*[@text='自选' and contains(@resource-id, 'tab_name')]").click()
         self.driver.find_element_by_id(
@@ -41,9 +54,14 @@ class testXueqiu(unittest.TestCase):
             "com.xueqiu.android:id/search_input_text").send_keys("alibaba")
         self.driver.find_element_by_id("com.xueqiu.android:id/follow_btn")
         self.driver.find_element_by_id("com.xueqiu.android:id/follow_btn").click()
-        # self.driver.find_element_by_xpath("//*[@text='下次再说']").click()
+        self.driver.find_element_by_xpath("//*[@text='下次再说']").click()
+        print(self.driver.find_element_by_id("com.xueqiu.android:id/add_attention") \
+              .find_element_by_class_name("android.widget.TextView").get_attribute("resource-id"))
         self.driver.find_element_by_id("com.xueqiu.android:id/action_close").click()
-        elText = self.driver.find_element_by_id("com.xueqiu.android:id/portfolio_stockName")
-        assert elText.text == "阿里巴巴"
+        try:
+            self.driver.find_element_by_id("com.xueqiu.android:id/iv_close").click()
+        finally:
+            elText = self.driver.find_element_by_id("com.xueqiu.android:id/portfolio_stockName")
+            assert elText.text == "阿里巴巴"
 
 
